19da8ed721046473a44519b5c3d62581cda066bb
Kyumin Kim <math4tots@users.noreply.github.com>
2020-08-23 10:26:15 -0700

    follow the guide a bit; little tweaks
    Looks like I need a canary or dev version of Edge for this to work

--- GettingStartedGuides/Win32_GettingStarted/HelloWebView.cpp
+++ GettingStartedGuides/Win32_GettingStarted/HelloWebView.cpp
@@ -2,12 +2,15 @@
 
 #include <windows.h>
 #include <stdlib.h>
+#include <iostream>
 #include <string>
 #include <tchar.h>
 #include <wrl.h>
 #include <wil/com.h>
 // include WebView2 header
+#include "WebView2.h"
 
+using namespace std;
 using namespace Microsoft::WRL;
 
 // Global variables
@@ -28,6 +31,11 @@ static wil::com_ptr<ICoreWebView2Controller> webviewController;
 
 // Pointer to WebView window
 static wil::com_ptr<ICoreWebView2> webviewWindow;
+
+int main()
+{
+	return WinMain(GetModuleHandle(NULL), NULL, GetCommandLineA(), SW_SHOWNORMAL);
+}
 
 int CALLBACK WinMain(
 	_In_ HINSTANCE hInstance,
@@ -104,9 +112,53 @@ int CALLBACK WinMain(
 	UpdateWindow(hWnd);
 
 	// <-- WebView2 sample code starts here -->
+	// Step 3 - Create a single WebView within the parent window
+	// Locate the browser and set up the environment for WebView
+	cout << "CreateCoreWebView... being called" << endl;
+	auto r = CreateCoreWebView2EnvironmentWithOptions(nullptr, nullptr, nullptr,
+		Callback<ICoreWebView2CreateCoreWebView2EnvironmentCompletedHandler>(
+			[hWnd](HRESULT result, ICoreWebView2Environment* env) -> HRESULT {
+				cout << "STARTING CALLBACK" << endl;
+				// Create a CoreWebView2Controller and get the associated CoreWebView2 whose parent is the main window hWnd
+				env->CreateCoreWebView2Controller(hWnd, Callback<ICoreWebView2CreateCoreWebView2ControllerCompletedHandler>(
+					[hWnd](HRESULT result, ICoreWebView2Controller* controller) -> HRESULT {
+						if (controller != nullptr) {
+							webviewController = controller;
+							webviewController->get_CoreWebView2(&webviewWindow);
+						}
 
+						// Add a few settings for the webview
+						// The demo step is redundant since the values are the default settings
+						ICoreWebView2Settings* Settings;
+						webviewWindow->get_Settings(&Settings);
+						Settings->put_IsScriptEnabled(TRUE);
+						Settings->put_AreDefaultScriptDialogsEnabled(TRUE);
+						Settings->put_IsWebMessageEnabled(TRUE);
 
-	
+						// Resize WebView to fit the bounds of the parent window
+						RECT bounds;
+						GetClientRect(hWnd, &bounds);
+						webviewController->put_Bounds(bounds);
+
+						// Schedule an async task to navigate to Bing
+						auto r = webviewWindow->Navigate(L"https://www.bing.com/");
+						cout << "AFTER NAVIGATE:" << r << endl;
+
+						// Step 4 - Navigation events
+
+						// Step 5 - Scripting
+
+						// Step 6 - Communication between host and web content
+
+						return S_OK;
+					}).Get());
+				return S_OK;
+			}).Get());
+
+	// r = 0x80070002 -> ERROR_FILE_NOT_FOUND
+	// this would most likely imply that the version of Edge that WebView2 wants isn't
+	// found
+	cout << "CreateCoreWebView... result = " << std::showbase << std::hex << r << endl;
 	// <-- WebView2 sample code ends here -->
 
 	// Main message loop:
